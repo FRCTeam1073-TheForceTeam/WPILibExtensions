332c71b3963fdf6b2b5d8446525b8997d91bbc10
Evin Ugur <evinoog96@gmail.com>
2013-02-08 21:48:01 -0500

    included derivative fix!

--- Commands/PrintStallData.h
+++ Commands/PrintStallData.h
@@ -1,10 +1,15 @@
 #ifndef PRINT_STALL_DATA_H
 #define PRINT_STALL_DATA_H
-#include "../../Robot.h"
+#include "WPILib.h"
 #include "Commands/Subsystem.h"
+#include "../Stallable.h"
 class PrintStallData : public Command{
 public:
 	PrintStallData(Stallable* stallable);
+	void Initialize(){}
+	void Execute(){}
 	bool IsFinished();
+	void End(){}
+	void Interrupted(){}
 };
 #endif
