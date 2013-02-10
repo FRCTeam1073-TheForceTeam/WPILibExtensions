479370aeb38176e978bfd36b5d0fda9f2e2f5dd5
Sanjay Cherian <scherian@charter.net>
2013-02-10 15:52:27 -0500

    made some methods public instead of protected

--- Stallable.h
+++ Stallable.h
@@ -12,11 +12,11 @@ public:
 	void PrintVoltages();
 	virtual float StallDetectLimit();	//if recorded data does not oscillate this much in volts we return false.
 	bool IsStall();
+	void ProcessVoltageData();
+	void ResetData();
 protected:
 	virtual float GetVoltageSource() = 0;
 	float voltageHistArray[VOLT_HISTORY_LEN];
-	void ProcessVoltageData();
-	void ResetData();
 private:
 	static const float stallDetectLimitVal = 0.3f;
 };
