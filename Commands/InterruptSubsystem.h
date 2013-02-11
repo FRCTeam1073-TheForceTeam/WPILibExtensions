289855d9a5c04502e5963bb02865e7b2cce07a81
Evin Ugur <evinoog96@gmail.com>
2013-02-11 16:18:45 -0500

    fixed include

--- Commands/InterruptSubsystem.h
+++ Commands/InterruptSubsystem.h
@@ -1,7 +1,7 @@
 #ifndef INTERRUPT_SUBSYSTEM_H
 #define INTERRUPT_SUBSYSTEM_H
 #include "Commands/Subsystem.h"
-#include "../../Robot.h"
+#include "WPILib.h"
 class InterruptSubsystem : public Command{
 public:
 	InterruptSubsystem(Subsystem* subsystem);
