8d651b1f4918a435f9d8f528a16b6329df8cb919
Evin Ugur <evinoog96@gmail.com>
2013-02-12 13:56:32 -0500

    Optimized Code by making LimitSwitch class an inline function

--- LimitSwitch.cpp
+++
@@ -1,8 +0,0 @@
-#include "LimitSwitch.h"
-LimitSwitch::LimitSwitch(int CHANNEL):
-	DigitalInput(CHANNEL) {}
-LimitSwitch::LimitSwitch(int MODULE, int CHANNEL): 
-	DigitalInput(MODULE, CHANNEL){}
-bool LimitSwitch::IsPressed(){
-	return Get() == 1;
-}
--- LimitSwitch.h
+++ LimitSwitch.h
@@ -1,11 +1,5 @@
-//class to avoid confusion when getting the value from a limit switch.
 #ifndef LIMIT_SWITCH_H
 #define LIMIT_SWITCH_H
-#include "wpilib.h"
-class LimitSwitch : public DigitalInput{
-public:
-	LimitSwitch(int CHANNEL);
-	LimitSwitch(int MODULE, int CHANNEL);
-	bool IsPressed();
-};
+#include "WPILib.h"
+inline bool LimitPressed(DigitalInput* input) {return input->Get() == 0;}
 #endif
