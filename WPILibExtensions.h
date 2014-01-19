295566d4a8e8ceb96e9a68e58b606bc508b1cd5a
Evin Ugur <evinoog96@gmail.com>
2014-01-19 11:30:28 -0500

    included new AnalogPressureTransducer class

--- WPILibExtensions.h
+++ WPILibExtensions.h
@@ -9,6 +9,7 @@
 #include "OnOff.h"
 #include "IREncoder.h"
 #include "SmartCANJaguarSeries.h"
+#include "AnalogPressureTransducer.h"
 #include "Commands/PrintStallData.h"
 #include "Commands/InterruptSubsystem.h"
 #include "Commands/ChangeJoystickModeCommand.h"
