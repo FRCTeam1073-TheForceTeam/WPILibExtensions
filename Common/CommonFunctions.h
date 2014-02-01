dd74ef92ddd7c237334617705be06b1c1730b925
Evin Ugur <evinoog96@gmail.com>
2014-02-01 16:09:17 -0500

    array length preprocessor

--- Common/CommonFunctions.h
+++ Common/CommonFunctions.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_FUNCTIONS_H
 #define COMMON_FUNCTIONS_H
 #define PI 3.14159f
+#define ARRAY_LEN(arr) (int)(sizeof(arr)/sizeof(arr[0]))
 inline float RadiansToDegrees(float radians){return radians * 180 / PI;}
 inline float DegreesToRadians(float degrees){return degrees * PI / 180;}
 #endif
