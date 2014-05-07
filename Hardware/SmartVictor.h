f4ed7265c8e68f4c47b4923927e1878c1e8c7b87
Evin Ugur <evinoog96@gmail.com>
2014-05-07 19:58:37 -0400

    publically define constructor and set

--- Hardware/SmartVictor.h
+++ Hardware/SmartVictor.h
@@ -3,6 +3,7 @@
 #include "Victor.h"
 #include "Invertable.h"
 class SmartVictor : public Victor, public Invertable {
+public:
 	SmartVictor(int module, int channel, bool isInverted = false);
 	void Set(float value);
 };
